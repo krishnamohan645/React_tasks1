a438d1c91e47e4c13d15d1b119815d37fb23157d
krishnamohan645 <krishnaguduri08@gmail.com>
2024-04-13 21:10:03 +0530

    mounting_phase

--- react_application/src/App.js
+++ react_application/src/App.js
@@ -1,10 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
+import ProductListing from './components/class-components/lifeCycle/mounting/productListing';
+// import CustomSpinner from './components/class-components/lifeCycle/mounting/spinner';
 
 function App() {
   return (
     <div className="App">
-
+      {/* <CustomSpinner/> */}
+      <ProductListing/>
     </div>
   );
 }
