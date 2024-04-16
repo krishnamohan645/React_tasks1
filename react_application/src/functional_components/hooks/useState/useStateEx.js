862b007cae3b7b82bc12486c04b1e43273a1e293
krishnamohan645 <krishnaguduri08@gmail.com>
2024-04-16 14:50:10 +0530

    usestate

--- react_application/src/functional_components/hooks/useState/useStateEx.js
+++ react_application/src/functional_components/hooks/useState/useStateEx.js
@@ -1,62 +1,78 @@
-import {  useState } from "react";
+import { useState } from "react";
 import React from "react";
 import axios from "axios";
-import "./useState.css"
+import "./useState.css";
 
-const UseStateExample=()=>{
-    const [products, setProducts] = useState([])
+const UseStateExample = () => {
+  const [products, setProducts] = useState([]);
 
+  const updateProduct = (
+    index,
+    updatedTitle,
+    updatedThumbnail,
+    updatedPrice
+  ) => {
+    const updatedProducts = products.map((eachObject, i) => {
+      if (i === index) {
+        return {
+          ...eachObject,
+          thumbnail: updatedThumbnail,
+          title: updatedTitle,
+          price: updatedPrice,
+        };
+      } else {
+        return eachObject;
+      }
+    });
+    console.log(updatedProducts);
+    setProducts(updatedProducts);
+  };
 
-    // updateItem = (index, updatedTitle, updatedThumbnail) => {
-    //     const newProducts = [...this.state.products];
-    //     if (index >= 0 && index < newProducts.length) {
-    //       newProducts[index] = {
-    //         ...newProducts[index],
-    //         title: updatedTitle,
-    //         thumbnail: updatedThumbnail
-    //       };
-    //       this.setState({ products: newProducts });
-    //     }
-    //   };
-   const updateProduct=(index,updatedTitle, updatedThumbnail,updatedPrice)=>{
-    const updatedProducts = [...products]
-    if(index > 0 && index < products.length){
-        updatedProducts[index]={
-            ...updatedProducts[index],
-            title: updatedTitle,
-            thumbnail: updatedThumbnail,
-            price: updatedPrice
-        }
-    }
-    console.log(updatedProducts)
-    }
-    
-    const fetchData = async()=>{
-        const result = await axios.get("https://dummyjson.com/products")
-        console.log(result)
-        setProducts(result.data.products)
-    }
-    useState(()=>{
-        fetchData()
-    },[])
-    return(
-        <>
-        <div className="container">
-           {
-            products.map((eachObject,index)=>{
-                return(
-                    <div className="cards" key={index}>
-                    <img src={eachObject.thumbnail} alt={eachObject.title} width={250} height={250}/>
-                    <h3 key={index}>{eachObject.title}</h3>
-                    <h3>₹{eachObject.price}</h3>
-                    <button>Delete</button>
-                    <button onClick={updateProduct} style={{marginLeft:"10"}}>Update</button>
-                    </div>
-                )
-            })
-           }
-        </div>
-        </>
-    )
-}
+  const deleteProduct = products.map()
+
+  const fetchData = async () => {
+    const result = await axios.get("https://dummyjson.com/products");
+    console.log(result);
+    setProducts(result.data.products);
+  };
+  useState(() => {
+    fetchData();
+  }, []);
+  return (
+    <>
+      <div className="container">
+        {products.map((eachObject, index) => {
+          return (
+            <div className="cards" key={index}>
+              <img
+                src={eachObject.thumbnail}
+                alt={eachObject.title}
+                width={250}
+                height={250}
+              />
+              <h3 key={index}>{eachObject.title}</h3>
+              <h3>₹{eachObject.price}</h3>
+              <button onClick={()=>{
+                deleteProduct()
+              }}>Delete</button>
+              <button
+                onClick={() =>
+                  updateProduct(
+                    index,
+                    products[1].title,
+                    products[1].thumbnail,
+                    products[1].price
+                  )
+                }
+                style={{ marginLeft: "10" }}
+              >
+                Update
+              </button>{" "}
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
 export default UseStateExample;
