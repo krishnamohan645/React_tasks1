31bf6139e6f0f4d2366bb157b6d6ab5e84c18e3c
krishnamohan645 <krishnaguduri08@gmail.com>
2024-04-21 20:40:41 +0530

    useEffect

--- react_application/src/App.js
+++ react_application/src/App.js
@@ -3,20 +3,10 @@
 
 import UseEffectProducts from "./useEffect/productListing";
 
-// import LoginFromPostApi from "./forms/controlled/post_form";
-
-// import ControlledInput from "./forms/controlled/controlled";
-// import ControlledComponents1 from "./forms/controlled/controlled1";
-
-// import UnControlledInput from './forms/unControlled/unControlled';
-
 function App() {
   return (
     <div className="App">
-      {/* <UnControlledInput/> */}
-      {/* <ControlledInput/> */}
-      {/* <ControlledComponents1/> */}
-      {/* <LoginFromPostApi/> */}
+
       <UseEffectProducts/>
     </div>
   );
---
+++ react_application/src/useEffect/productListing.js
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./index.css";
+
+const UseEffectProducts = () => {
+  const [data, setData] = useState([]);
+  const [eachObject, setEachObject] = useState({});
+  const [count, setCount] = useState(1);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    gettingSingleData();
+  }, [count]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("https://dummyjson.com/products");
+      setData(response.data.products); // Update state with products array
+      console.log('response: ', response);
+    } catch (error) {
+      console.log("Error fetching data: ", error);
+    }
+  };
+
+  const gettingSingleData = async () => {
+    try {
+      const result = await axios.get(`https://dummyjson.com/products/${count}`);
+      setEachObject(result.data); // Update state with single product object
+      console.log('result: ', result);
+    } catch (error) {
+      console.log("Not Getting single Item:", error);
+    }
+  };
+
+  const handleButtonClick = (id) => {
+    setCount(id);
+  };
+
+  return (
+    <div className="container">
+      <h1 className="title">Product Viewer</h1>
+      <div className="product-list">
+        {data.map((product) => (
+          <button
+            key={product.id}
+            className={`product-button ${
+              count === product.id ? "active" : ""
+            }`}
+            onClick={() => handleButtonClick(product.id)}
+          >
+            {product.id}
+          </button>
+        ))}
+      </div>
+      <div className="card">
+        <h1>{eachObject.title}</h1>
+        <img src={eachObject.thumbnail} alt={eachObject.title} />
+        <p className="description">{eachObject.description}</p>
+        <h2>Price: {eachObject.price}</h2>
+      </div>
+    </div>
+  );
+};
+
+export default UseEffectProducts;
