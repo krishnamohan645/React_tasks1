8d627fa37c3d6eb5b927f352676658cb57eb687b
krishnamohan645 <krishnaguduri08@gmail.com>
2024-04-21 20:55:51 +0530

    navigation

--- react_application/src/App.js
+++ react_application/src/App.js
@@ -1,13 +1,11 @@
 // import logo from './logo.svg';
 // import './App.css';
 
-import UseEffectProducts from "./useEffect/productListing";
 
 function App() {
   return (
     <div className="App">
 
-      <UseEffectProducts/>
     </div>
   );
 }
